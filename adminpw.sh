09da67a0c4554d9d02ae69bbb76f2e66719d92ce
d <d@l>
2026-08-20 14:21:10 +0800

    adminpw-filter

--- adminpw.sh
+++ adminpw.sh
@@ -6,9 +6,11 @@ for spec in "peluang88-my.com" "12cucimy.com" "mantap88a.my"; do
   php -r '
   include "'"$D"'/data/common.inc.php";
   $m=new mysqli($cfg_dbhost,$cfg_dbuser,$cfg_dbpwd,$cfg_dbname); if($m->connect_errno){echo "'"$spec"' dbfail\n";exit;}
-  $p=getenv("PW"); $old=substr(md5($p),5,20); $new=password_hash($p,PASSWORD_BCRYPT);
+  $raw=getenv("PW");
+  $p=preg_replace("/[^0-9a-zA-Z_@!.\\-]/","",$raw);   // same filter DedeBIZ applies at login
+  $old=substr(md5($p),5,20); $new=password_hash($p,PASSWORD_BCRYPT);
   $tbl=$cfg_dbprefix."admin";
-  $m->query("UPDATE `$tbl` SET pwd=\x27$old\x27, pwd_new=\x27".$m->real_escape_string($new)."\x27, userid=\x27admin\x27 WHERE id=1 OR userid=\x27admin\x27");
-  echo "'"$spec"' rows=".$m->affected_rows."\n";
+  $m->query("UPDATE `$tbl` SET pwd=\x27$old\x27, pwd_new=\x27".$m->real_escape_string($new)."\x27, userid=\x27admin\x27, loginerr=0 WHERE id=1 OR userid=\x27admin\x27");
+  echo "'"$spec"' rows=".$m->affected_rows." effpw=".$p."\n";
   '
 done > "$B/peluang88-my.com/public_html/_adminpw.txt" 2>&1
