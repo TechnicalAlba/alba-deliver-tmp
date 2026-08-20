2ca9cd3db50eb7796322d018296e9ac226645929
d <d@l>
2026-08-20 14:15:44 +0800

    checkadmin2

--- checkadmin.sh
+++ checkadmin.sh
@@ -4,8 +4,8 @@ php -r '
 include "'"$D"'/data/common.inc.php";
 $m=new mysqli($cfg_dbhost,$cfg_dbuser,$cfg_dbpwd,$cfg_dbname); if($m->connect_errno){echo "dbfail\n";exit;}
 $t=$cfg_dbprefix."admin";
-echo "prefix=$cfg_dbprefix table=$t\n";
-$r=$m->query("SELECT id,userid,LENGTH(pwd) pl,LENGTH(pwd_new) pnl,uarank FROM `$t`");
+$r=$m->query("SELECT * FROM `$t`");
 if(!$r){echo "queryerr: ".$m->error."\n"; exit;}
-while($a=$r->fetch_assoc()){ echo "id=".$a["id"]." userid=".$a["userid"]." pwd_len=".$a["pl"]." pwdnew_len=".$a["pnl"]." rank=".$a["uarank"]."\n"; }
+echo "rows=".$r->num_rows."\n";
+while($a=$r->fetch_assoc()){ echo "id=".$a["id"]." userid=".$a["userid"]."\n"; }
 ' > "$D/_admincheck.txt" 2>&1
