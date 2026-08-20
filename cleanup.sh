0ee4e420bff5c5834b75d601457da5ddb026c8da
d <d@l>
2026-08-20 14:23:15 +0800

    cleanup2

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 B=/home/u133275855/domains
 for d in peluang88-my.com 12cucimy.com mantap88a.my; do
-  rm -f $B/$d/public_html/_log.txt $B/$d/public_html/_gen.txt $B/$d/public_html/_clone.txt $B/$d/public_html/_htaccess.txt $B/$d/public_html/index.php.bak 2>/dev/null
+  rm -f "$B/$d/public_html"/_adminpw.txt "$B/$d/public_html"/_admincheck.txt "$B/$d/public_html"/_logintest.txt "$B/$d/public_html"/_gen.txt "$B/$d/public_html"/_clone.txt "$B/$d/public_html"/_cleanup.txt "$B/$d/public_html"/_htaccess.txt "$B/$d/public_html"/index.php.bak 2>/dev/null
 done
-echo "cleanup done $(date)" > $B/peluang88-my.com/public_html/_cleanup.txt
+echo "cleaned $(date)" > "$B/peluang88-my.com/public_html/_ok.txt"
